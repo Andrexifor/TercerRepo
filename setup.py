4c079a5aa6a177fc97763716e8d0d8cb11b836e4
Felix Andre <andre_12_aries@hotmail.com>
2026-04-26 15:00:43 -0500

    primer lanzamiento de pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                                  # Nombre del paquete
+    version="0.1.0",                                    # Versión inicial
+    packages=find_packages(),                           # Paquetes a incluir
+    description="Un paquete pip simple de saludo",      # Breve descripción
+    author="Felix Andre",                               # Tu nombre
+    author_email="andrexifor@gmail.com",                # Tu correo electrónico
+    url="https://github.com/Andrexifor/TercerRepo",     # URL del proyecto
+)
